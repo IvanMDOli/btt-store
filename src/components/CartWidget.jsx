fdbd90ace82915a1a1f5086c44e7a1a604246e66
IvanMDOli <ivanmdeoli@hotmail.com>
2023-11-19 20:45:07 -0300

    Segundo Commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartWidget() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default CartWidget;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import CartWidget from './CartWidget'
 import '../styles/navbar.css'
 import logoStore from '../assets/BackToTheStore.png'
+import carrito from '../assets/carrito.png'
 
 function NavBar() {
     return (
@@ -14,6 +16,7 @@ function NavBar() {
                     <li>Vehiculos</li>
                     <li>Ropa</li>
                 </ul>
+                <img className='carrito' rel="carrito-icono" src={carrito} />
                 <ul className='botones-principales'>
                     <li>Inicio</li>
                     <li>Login</li>
